c21219d526cbd1c4488e4acdb8c26e66a3d64a19
Omidp <omidpourhadi@gmail.com>
2016-04-24 13:32:36 +0430

    add requireJS

---
+++ requireJS/scripts/main.js
@@ -0,0 +1,11 @@
+
+requirejs.config({
+    baseUrl: 'helper',
+    paths: {
+        app: './'
+    }
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+//requirejs(['main']);
