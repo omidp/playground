3b1b34c0ecf1a85a7846027f5fec51c250e2147f
Omidp <omidpourhadi@gmail.com>
2016-05-09 16:34:23 +0430

    add nodejs cluster

--- nodejs/cluster.js
+++ nodejs/cluster.js
@@ -1,17 +1,36 @@
 var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
-console.log('number od cpu(s) : ' + numCPUs);
+var workers = {};
+var requests = 0;
 if (cluster.isMaster) {
 	for (var i = 0; i < numCPUs; i++) {
-	cluster.fork();
+		workers[i] = cluster.fork();
+		(function (i) {
+			workers[i].on('message', function(message) {
+			if (message.cmd == 'incrementRequestTotal') {
+				requests++;
+			for (var j = 0; j < numCPUs; j++) {
+				workers[j].send({cmd:'updateOfRequestTotal',requests: requests});
+			}
+			}
+});
+})(i);
 	}
 	cluster.on('exit', function(worker, code, signal) {
 		console.log('Worker ' + worker.process.pid + ' died.');
 	});
 } else {
+	process.on('message', function(message) {
+		if (message.cmd == 'updateOfRequestTotal') {
+		requests = message.requests;
+		}
+	});
 http.Server(function(req, res) {
-	res.writeHead(200);
-	res.end('I am a worker running in process ' + process.pid);
-}).listen(8000);
+res.writeHead(200);
+res.end('Worker in process ' + process.pid
++ ' says cluster has responded to ' + requests
++ ' requests.');
+process.send({cmd: 'incrementRequestTotal'});
+}).listen(3000);
 }
